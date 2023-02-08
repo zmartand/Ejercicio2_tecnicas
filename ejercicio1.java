bf1baf06f257c9f91c922af091db07164c31609d
zmartand <113374358+zmartand@users.noreply.github.com>
2023-02-08 16:26:34 +0000

    intento1

---
+++ ejercicio1.java
@@ -0,0 +1,12 @@
+/*Cuáles serán los valores de las variables a y b después de cada línea del siguiente algoritmo?
+
+Algo ValoresDeLasVariables
+variable a, b: entero
+Inicio
+a <- 3
+escribir("a = " & a)
+b<-a+5
+escribir("a = " & a & " y b = " & b)
+a<-7
+escribir("a = " & a & " y b = " & b)*/
+
