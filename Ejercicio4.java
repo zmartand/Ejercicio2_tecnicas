4588892571d7bc64c0d6137babbd095eef783954
zmartand <113374358+zmartand@users.noreply.github.com>
2023-02-11 00:37:11 +0100

    Update Ejercicio4.java
    ej4 corregido y comentado

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -8,12 +8,14 @@ Introduzca la distancia recorrida (km).
 Introduzca el tiempo del recorrido (min).
 240
 Se ha desplazado a una velocidad de 92,5 km/h.*/
-public static void main(String[] args) {
-        double distancia, tiempo, velocidad;
-        distancia = Double.parseDouble(JOptionPane.showInputDialog("Introduzca la distancia recorrida (km)."));
-        tiempo = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el tiempo del recorrido (min)."));
-        velocidad = distancia / tiempo;
-        JOptionPane.showMessageDialog(null, "Se ha desplazado a una velocidad de " + velocidad + " km/h.");
+    
+    
+    public static void main(String[] args) {
+        double distancia, tiempo, velocidad; //Declaramos las 3 variables necesarias para calcular la velocidad
+        distancia = Double.parseDouble(JOptionPane.showInputDialog("Introduzca la distancia recorrida (kilometros).")); //Pedimos al usuario que introduzca la distancia recorrida en km
+        tiempo = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el tiempo del recorrido (minutos).")); //Pedimos al usuario que introduzca el tiempo del recorrido en minutos
+        velocidad = (distancia / tiempo) * 60;//Realizamos el cálculo de la velocidad y los minutos los pasamos a horas para que el resultado final sea en km/h
+        JOptionPane.showMessageDialog(null, "Se ha desplazado a una velocidad de " + velocidad + " km/h."); //Informamos sobre el resultado
     }
-
 }
+
