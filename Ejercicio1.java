5271f46ce3806095054a5255237975b8958b932f
zmartand <113374358+zmartand@users.noreply.github.com>
2023-02-11 00:30:58 +0100

    Update Ejercicio1.java
    sol ej1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -20,3 +20,7 @@ public class Ejercicio1 {
     System.out.println("a = " + a + " y b = " + b); //En esta línea de código a vale 7 y b vale 8
     } 
     }
+/*Solución: 
+a = 3
+a = 3 y b = 8
+a = 7 y b = 8*/
